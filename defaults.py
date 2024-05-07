416df95f12650b558cf8cc181eea9ba809ec1b86
Shubhangi Vashist <165549865+ShubhangiiVashist@users.noreply.github.com>
2024-05-07 20:20:15 +0530

    Create defaults.py

---
+++ defaults.py
@@ -0,0 +1,7 @@
+DEFAULT_SUBSCRIPTION = ""                                      #enter your subscription ID
+DEFAULT_RESOURCE_GROUP = ""                                    #enter your name of your resource group
+DEFAULT_CONTAINER_NAME = "my-blob-container"                   #change the container as per choice
+DEFAULT_LOCATION = "eastus"                                    #change the default location as per choice
+DEFAULT_PRINCIPLE_ID = ""                                      #enter your principle/object ID
+DEFAULT_QUEUE_NAME = "my-queue"                                #change the queue name as per choice
+DEFAULT_TABLE_NAME = "mytable"                                 #change the table name as per choice
