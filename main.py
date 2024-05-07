7fbfbafd6d81157d33632558397fc2d67173c46c
Shubhangi Vashist <165549865+ShubhangiiVashist@users.noreply.github.com>
2024-05-07 20:16:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Importing necessary libraries 
+
 from azure.mgmt.authorization import AuthorizationManagementClient
 from azure.mgmt.authorization.models import RoleAssignmentCreateParameters
 from azure.identity import *
@@ -6,6 +7,7 @@ from azure.mgmt.storage import StorageManagementClient
 from azure.storage.blob import BlobClient,BlobServiceClient
 from azure.storage.queue import QueueClient,QueueMessage, QueueServiceClient
 from azure.data.tables import TableServiceClient
+
 import random
 import ast
 import uuid
@@ -13,20 +15,18 @@ import jwt
 import time
 import defaults
 
-#Creating a storage account with a unique name
+
 def create_storage_account_and_container(resource_group,credential,subscription_id):
-     #Setting up storage management client
-    storage_client = StorageManagementClient(credential,subscription_id)
-    #Checking the availability of the storage group name
+     
+    storage_client = StorageManagementClient(credential,subscription_id)                     #Setting up storage management client
     var= False
     while var != True:
-        Base_name = f'{'teststoreaccount'}{random.randint(1,100)}'
+        Base_name = f'{'teststoreaccount'}{random.randint(1,100)}'                           #Checking the availability of the storage group name
         result = storage_client.storage_accounts.check_name_availability({'name':Base_name})
         var = result.name_available
 
-    #Creating storage group with a unique name
     poller = storage_client.storage_accounts.begin_create(
-        resource_group_name = resource_group, account_name = Base_name , 
+        resource_group_name = resource_group, account_name = Base_name ,                      #Creating storage group with a unique name
         parameters = { 'location': defaults.DEFAULT_LOCATION , 'kind': 'StorageV2' , 'sku' : {'name' : 'Standard_LRS'}
                     }
     )
@@ -35,20 +35,16 @@ def create_storage_account_and_container(resource_group,credential,subscription_
     storage_account =  poller_Results.name
 
     #Creating a storage Container for blob
+     
     container_name = defaults.DEFAULT_CONTAINER_NAME
-    containter = storage_client.blob_containers.create(
+    container = storage_client.blob_containers.create(
         resource_group_name = resource_group,
         account_name = storage_account,
         container_name = container_name,
-        blob_container = {"metadata": 
-                          {
-                "category": "documents",
-                "owner": "floating nimbus limited"
-                          }
-                        }
+        blob_container = {"metadata": { "category": "documents", "owner": "cloud owner details" } }
     )
-    #Assigning relevant roles to storage accounts
-    role_assignment(storage_account)
+    
+    role_assignment(storage_account)                                                             #Assigning relevant roles to storage accounts
     return storage_account
 
 
@@ -61,8 +57,7 @@ def role_assignment(storage_account):
     guid_str_3 = str(new_guid_3)
     credential = DefaultAzureCredential()
 
-    #Setting up authorization management client
-    authorization_client = AuthorizationManagementClient(credential, subscription_id)
+    authorization_client = AuthorizationManagementClient(credential, subscription_id)             #Setting up authorization management client
     principal_id = defaults.DEFAULT_PRINCIPLE_ID 
     scope = f"/subscriptions/{subscription_id}/resourceGroups/{resource_group}/providers/Microsoft.Storage/storageAccounts/{storage_account}"
 
@@ -87,18 +82,16 @@ def role_assignment(storage_account):
     role_assignment_2 = authorization_client.role_assignments.create(scope=scope, role_assignment_name = guid_str_2, parameters=role_assignment_params_2)
     role_assignment_3 = authorization_client.role_assignments.create(scope=scope, role_assignment_name = guid_str_3, parameters=role_assignment_params_3)
 
-#Upload file to the Blob storage
-def upload_blob(storage_account):
-    print("Upload any Document")
+def upload_blob(storage_account):   
+    print("Upload any Document")                                                        #Upload file to the Blob storage
     path = input("Path for Document: ")
     account_url = "https://"+storage_account+".blob.core.windows.net/"
     
-    #Extracting username for naming convention
-    token_bytes = credential.get_token(account_url).token
+    token_bytes = credential.get_token(account_url).token                               #Extracting username for naming convention
     decoded_token = jwt.decode(token_bytes, options={"verify_signature": False})
     user_name = decoded_token.get("name")
     
-    #Generating a unique naming convention for document, as per the user
+    #Generating a unique naming convention for blob to upload
     var = True
     while var!= False:
         document_name = f'{user_name}{"-document-"}{random.randint(1,1000)}'
@@ -111,35 +104,34 @@ def upload_blob(storage_account):
                 blob_client.upload_blob(data)
             var = False    
             print (f'{"Document with convention "}{document_name}{" successfully created"}')
-    blob_properties = blob_client.get_blob_properties()
+             
+    blob_properties = blob_client.get_blob_properties()                                   #Creating a dictionary of metadata to push in the queue
     dt = blob_properties.creation_time
     date_time_string = dt.strftime("%Y-%m-%d %H:%M:%S")
+     
     dict = {"name" : user_name , "time" : date_time_string,"url":f"{blob_service_client.url}{container_name}/{document_name}"} 
     return (dict)
 
-#Create a Queue and upload the upload to the blob storage
 def queue_upload(storage_account,credential):
-    #Queue client setup
-    queue_client= QueueClient(
+    queue_client= QueueClient(                                                              #Queue client setup
         account_url= "https://"+storage_account+".queue.core.windows.net/", 
         queue_name = defaults.DEFAULT_QUEUE_NAME ,credential= credential )
-    #Create a Queue
     queue_client.create_queue()
-    #Enqueue
+
     enqueue = "yes"
     while enqueue != "no":
         blob_data = upload_blob(storage_account)
         queue_client.send_message(blob_data)
         enqueue = input("Upload another file?")
 
-#Dequeue to the table at the rate of 1 req/min by invoking job_processor 
 def dequeue(storage_account,credential,connection_string):
-    #Get messsage off the queue
-    table_client  = TableServiceClient.from_connection_string(conn_str=connection_string)
+    table_client  = TableServiceClient.from_connection_string(conn_str=connection_string)     #Dequeue to the table at the rate of 1 req/min by invoking job_processor 
     table = table_client.create_table(table_name = defaults.DEFAULT_TABLE_NAME)
+     
     queue_client= QueueClient(
             account_url= "https://"+storage_account+".queue.core.windows.net/", 
                 queue_name = defaults.DEFAULT_QUEUE_NAME ,credential= credential )
+     
     while queue_client.get_queue_properties().approximate_message_count != 0:
         message = queue_client.receive_message()
         if message != None: 
@@ -150,8 +142,8 @@ def dequeue(storage_account,credential,connection_string):
 
 def job_processor(message,connection_string,table):
     string_data = message['content']
-    # Initialize the TableServiceClient with the connection string
-    table_client  = TableServiceClient.from_connection_string(conn_str=connection_string)
+    
+    table_client  = TableServiceClient.from_connection_string(conn_str=connection_string)      # Initialize the TableServiceClient with the connection string
     data = ast.literal_eval(string_data)
     table_entity=table.create_entity(
     entity= {'PartitionKey':data['name'],
@@ -164,13 +156,13 @@ if __name__ == "__main__":
     subscription_id = defaults.DEFAULT_SUBSCRIPTION
     resource_group = defaults.DEFAULT_RESOURCE_GROUP
 
-    #Setting up credentials
-    credential = AzureCliCredential()
-    #Create a storage account 
-    storage_account = create_storage_account_and_container(resource_group,credential,subscription_id)
+    credential = AzureCliCredential()                                                            #Setting up credentials
+
+    storage_account = create_storage_account_and_container(resource_group,credential,subscription_id)  #Create a storage account 
     
-    connection = input("enter key")
-    connection_string = input("enter string")
+    connection = input("enter connection key")
+    connection_string = input("enter connection string")
+     
     queue_upload(storage_account,connection)
     dequeue(storage_account,connection,connection_string)
     
